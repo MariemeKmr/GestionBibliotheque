8f86ceabbbcab78cd21889c99d112a7fdf223f85
MariemeKmr <Mariemekamara04@gmail.com~>
2026-06-17 16:32:52 +0000

    feat: configuration initiale Spring Boot

---
+++ src/main/java/com/marieme/bibliotheque/GestionBibliothequeApplication.java
@@ -0,0 +1,16 @@
+package com.marieme.bibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Point d'entree de l'application de gestion de bibliotheque.
+ * Auteur : Marieme Kamara
+ */
+@SpringBootApplication
+public class GestionBibliothequeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionBibliothequeApplication.class, args);
+    }
+}
